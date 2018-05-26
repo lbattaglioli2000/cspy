044cffc666863097d53ef19046ba22f35726d4dd
Luigi Battagllioli <lbattaglioli2000@gmail.com>
2018-05-26 16:09:05 -0400

    added analytics

--- resources/views/student/layouts/master.blade.php
+++ resources/views/student/layouts/master.blade.php
@@ -42,3 +42,13 @@
 <script>
     feather.replace()
 </script>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-108145541-2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-108145541-2');
+</script>
