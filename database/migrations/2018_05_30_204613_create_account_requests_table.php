5c5701ac4d642669282039df193bafabf7ab8ba4
Luigi Battaglioli <lbattaglioli2000@gmail.com>
2018-06-02 12:06:01 -0400

    updated account_request migration

--- database/migrations/2018_05_30_204613_create_account_requests_table.php
+++ database/migrations/2018_05_30_204613_create_account_requests_table.php
@@ -18,6 +18,7 @@ class CreateAccountRequestsTable extends Migration
             $table->text('name');
             $table->string('email')->unique();
             $table->timestamps();
+            
         });
     }
 
