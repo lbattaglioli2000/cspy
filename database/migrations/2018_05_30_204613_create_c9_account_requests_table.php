bdbb8ec1a7d72c5a2e3d855ee716e6c8d0eed2fa
Luigi Battaglioli <lbattaglioli2000@gmail.com>
2018-06-03 16:08:01 -0400

    edited class name to correspond with filename.

--- database/migrations/2018_05_30_204613_create_c9_account_requests_table.php
+++ database/migrations/2018_05_30_204613_create_c9_account_requests_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAccountRequestsTable extends Migration
+class CreateC9AccountRequestsTable extends Migration
 {
     /**
      * Run the migrations.
