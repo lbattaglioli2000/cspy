b8b3a080487a3d40cdd66661343f7c819aee6d3a
Luigi Battaglioli <luigi@fundamentalshape.com>
2018-12-03 23:31:35 -0500

    removed search

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,61 +1,5 @@
 <header class="header" id="header-main">
 
-    <!-- Search -->
-    <div id="search-main" class="navbar-search">
-        <div class="container">
-            <!-- Search form -->
-            <form class="navbar-search-form" role="form">
-                <div class="form-group">
-                    <div class="input-group input-group-transparent">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text"><i class="fas fa-search"></i></span>
-                        </div>
-                        <input type="text" class="form-control" placeholder="Type and hit enter ...">
-                    </div>
-                </div>
-            </form>
-            <div class="navbar-search-suggestions">
-                <h6>Search Suggestions</h6>
-                <div class="row">
-                    <div class="col-sm-6">
-                        <ul class="list-unstyled mb-0">
-                            <li>
-                                <a class="list-link" href="#">
-                                    <i class="fas fa-search"></i>
-                                    <span>macbook pro</span> in Laptops
-                                </a>
-                            </li>
-                            <li>
-                                <a class="list-link" href="#">
-                                    <i class="fas fa-search"></i>
-                                    <span>iphone 8</span> in Smartphones
-                                </a>
-                            </li>
-                            <li>
-                                <a class="list-link" href="#">
-                                    <i class="fas fa-search"></i>
-                                    <span>macbook pro</span> in Laptops
-                                </a>
-                            </li>
-                            <li>
-                                <a class="list-link" href="#">
-                                    <i class="fas fa-search"></i>
-                                    <span>beats pro solo 3</span> in Headphones
-                                </a>
-                            </li>
-                            <li>
-                                <a class="list-link" href="#">
-                                    <i class="fas fa-search"></i>
-                                    <span>smasung galaxy 10</span> in Phones
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
     <!-- Main navbar -->
     <nav class="navbar navbar-main navbar-expand-lg navbar-sticky navbar-transparent navbar-dark bg-dark" id="navbar-main">
         <div class="container">
@@ -112,11 +56,6 @@
                         <li class="nav-item mr-2">
                             <a href="{{ route('register') }}" class="btn btn-sm btn-white btn-circle btn-icon d-none d-lg-inline-flex">
                                 <span class="btn-inner--text">Register</span>
-                            </a>
-                        </li>
-                        <li class="nav-item mr-0">
-                            <a href="#" data-action="search-open" data-target="#search-main" class="btn btn-sm btn-white btn-circle btn-icon d-none d-lg-inline-flex">
-                                <span class="btn-inner--text"><i class="fas fa-search"></i></span>
                             </a>
                         </li>
                     @endif
